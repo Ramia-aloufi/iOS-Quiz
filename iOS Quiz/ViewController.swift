279e053fbae3eb8841e2ed62c0bb9ea21a38ccce
RAM111111 <r.r.99@windowslive.com>
2021-12-04 22:48:32 +0300

    Initial Commit

---
+++ iOS Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Quiz
+//
+//  Created by R on 29/04/1443 AH.
+//  Copyright © 1443 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
